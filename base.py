cfe7238d10d09214891b638b9d802236442e9cd2
Justin <101justinbii@gmail.com>
2022-06-15 19:43:13 +0800

    add tic tac toe

---
+++ base.py
@@ -0,0 +1,165 @@
+import pygame
+import sys
+
+BG_COLOR = (36, 106, 115)
+HEIGHT = 600
+WIDTH = 600
+SCREEN_SIZE = (HEIGHT, WIDTH)
+GAME_TITLE = "Tic Tac Toe"
+STROKE_WIDTH = 5
+STROKE_COLOR = (255, 255, 255)
+SIGN_COLOR = (246, 174, 45)
+CROSS_MARGIN = 40
+
+class Board:
+    def __init__(self, n: int):
+        self.n = n
+
+class Player:
+    def __init__(self, sign: int):
+        self.sign = sign
+
+def draw_line():
+    pygame.draw.line(screen, STROKE_COLOR, (0, HEIGHT/3), (WIDTH, HEIGHT/3), STROKE_WIDTH)
+    pygame.draw.line(screen, STROKE_COLOR, (0, HEIGHT/3*2), (WIDTH, HEIGHT/3*2), STROKE_WIDTH)
+    pygame.draw.line(screen, STROKE_COLOR, (WIDTH/3, 0), (WIDTH/3, HEIGHT), STROKE_WIDTH)
+    pygame.draw.line(screen, STROKE_COLOR, (WIDTH/3*2, 0), (WIDTH/3*2, HEIGHT), STROKE_WIDTH)
+
+def draw_circle(row, col):
+    pygame.draw.circle(screen, SIGN_COLOR, (int(col * WIDTH/3 + WIDTH/6), int(row * HEIGHT/3 + HEIGHT/6)), HEIGHT/6 * 0.7, STROKE_WIDTH)
+
+    # [[1, 2, 3],
+    #  [4, 5, 6],
+    #  [7, 8, 9]]
+
+def draw_cross(row, col):
+    pygame.draw.line(screen,
+                     SIGN_COLOR,
+                     (int(col * WIDTH/3 + CROSS_MARGIN), int(row * HEIGHT/3 + CROSS_MARGIN)),
+                     (int(col * WIDTH/3 + WIDTH/3 - CROSS_MARGIN), int(row * HEIGHT/3 + HEIGHT/3 - CROSS_MARGIN)), 
+                     STROKE_WIDTH)
+    pygame.draw.line(screen,
+                     SIGN_COLOR,
+                     (int(col * WIDTH/3 + CROSS_MARGIN), int(row * HEIGHT/3 + HEIGHT/3 - CROSS_MARGIN)),
+                     (int(col * WIDTH/3 + WIDTH/3 - CROSS_MARGIN), int(row * HEIGHT/3 + CROSS_MARGIN)), 
+                     STROKE_WIDTH)
+#:%s/pattern/replace/g 
+
+def draw_sign(row, col):
+    if player == 1:
+        draw_circle(row, col)
+    elif player == 2:
+        draw_cross(row, col)
+
+def is_valid_move(row, col):
+    # 0 -> None
+    # 1 -> O
+    # 2 -> X
+    return board[row][col] == 0
+
+def create_board():
+    # [[1, 2, 3],
+    #  [4, 5, 6],
+    #  [7, 8, 9]]
+    board = []
+    for i in range(3):
+        row = []
+        
+        for j in range(3):
+            row.append(0)
+        
+        board.append(row)
+    return board    
+
+def move(row, col):
+    board[row][col] = player
+
+def is_player_win():
+    win = False
+    # check rows
+    for i in range(3):
+        win = True
+        for j in range(3):
+            if player != board[i][j]:
+                win = False
+                break
+        if win:
+            return win
+    
+    # check cols
+    for i in range(3):
+        win = True
+        for j in range(3):
+            if player != board[j][i]:
+                win = False
+                break
+        if win:
+            return win
+    # check diagonals
+    win = True
+    for j in range(3):
+        if player != board[j][j]:
+            win = False
+            break
+    if win:
+        return win
+    
+    win = True
+    for e, i in enumerate(list(range(2, -1, -1))):
+        if player != board[e][i]:
+            win = False
+            break
+    if win:
+        return win
+
+def is_board_filled():
+    # for i in range(3):
+    #     for j in range(3):
+    #         if board[i][j] == 0:
+    #             return False
+    # return True
+    for row in board:
+        for sign in row:
+            if sign == 0:
+                return False
+    return True 
+
+
+if "__main__" == __name__:
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption(GAME_TITLE)
+    screen.fill(BG_COLOR)
+    draw_line()
+    board = create_board()
+    player = 1
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos_x, pos_y = event.pos
+                col = int(pos_x // (WIDTH/3))
+                row = int(pos_y // (HEIGHT/3))
+                # print(row, col)
+                # draw_cross(row, col)
+                if is_valid_move(row, col):
+                    print("valid")
+                    move(row, col)
+                    draw_sign(row, col)
+                    
+                    if is_player_win():
+                        print(f"Player{player} wins")
+                        sys.exit()
+                    
+                    if is_board_filled():
+                        print("Tie")
+                        sys.exit()
+                    
+                    # player = 3 - player
+                    player = 2 if player == 1 else 1
+                else:
+                    print("fail")
+
+        pygame.display.update()            
---
+++ enhanced.py
@@ -0,0 +1,176 @@
+# import
+from pathlib import Path
+import pygame
+import random
+import sys
+import time
+
+# game settings
+ASSETS_DIR = "assets"
+BACKGROUND_IMAGE = "background.jpg"
+BOARD_IMAGE = "board.png"
+CIRCLE_IMAGE = "circle.png"
+CROSS_IMAGE = "cross.png"
+CROSSHAIR_IMAGE = "crosshair 2.png"
+CELL_DELTA = 195
+CELL_ORIGIN = 30
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+class Crosshair(pygame.sprite.Sprite):
+    def __init__(self, filename):
+        super().__init__()
+        self.image = pygame.image.load(Path(ASSETS_DIR) / filename)
+        self.image = pygame.transform.scale(self.image, (80, 80))
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
+
+class Sign(pygame.sprite.Sprite):
+    def __init__(self, filename, coordinate, size = 1, length = 150):
+        super().__init__()
+        self.image = pygame.image.load(Path(ASSETS_DIR) / filename)
+        self.image = pygame.transform.scale(self.image, (length, length))
+        self.rect = pygame.Rect(coordinate[0], coordinate[1], size, size)
+
+class Board:
+    def __init__(self, size = 600):
+        self.size = size
+        self.surface = pygame.image.load(Path(ASSETS_DIR) / BOARD_IMAGE)
+        self.surface = pygame.transform.scale(self.surface, (size, size))
+        self.status = create_2D()
+    
+    def is_valid_move(self, row, col):
+        return self.status[row][col] == 0
+
+    def move(self, row, col, player):
+        self.status[row][col] = player
+        return 2 if player == 1 else 1
+    
+    def is_player_win(self, player) -> bool:
+        win = False
+        # check rows
+        for i in range(3):
+            win = True
+            for j in range(3):
+                if player != self.status[i][j]:
+                    win = False
+                    break
+            if win:
+                return win
+        
+        # check cols
+        for i in range(3):
+            win = True
+            for j in range(3):
+                if player != self.status[j][i]:
+                    win = False
+                    break
+            if win:
+                return win
+        # check diagonals
+        win = True
+        for j in range(3):
+            if player != self.status[j][j]:
+                win = False
+                break
+        if win:
+            return win
+        
+        win = True
+        for e, i in enumerate(list(range(2, -1, -1))):
+            if player != self.status[e][i]:
+                win = False
+                break
+        if win:
+            return win
+    
+    def is_board_filled(self): 
+        for row in self.status:
+            for sign in row:
+                if sign == 0:
+                    return False
+        return True 
+
+def init_player(players):
+    return random.randint(*players)
+
+def create_2D():
+    # [[1, 2, 3],
+    #  [4, 5, 6],
+    #  [7, 8, 9]]
+    x = []
+    for i in range(3):
+        row = []
+        
+        for j in range(3):
+            row.append(0)
+        
+        x.append(row)
+    return x    
+
+if "__main__" == __name__:
+    running = True
+
+    # initialization
+    pygame.init()
+    pygame.mouse.set_visible(False)
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption("Tic Tac Toe")
+
+    background = pygame.image.load(Path(ASSETS_DIR) / BACKGROUND_IMAGE)
+    board = Board()
+    player = init_player([1, 2])
+    sign_mapping = {
+        1: CIRCLE_IMAGE,
+        2: CROSS_IMAGE
+    }
+    group = pygame.sprite.Group()
+    crosshair = Crosshair(CROSSHAIR_IMAGE)
+    group.add(crosshair)
+    # circle = Sign(CIRCLE_IMAGE, (100, 100))
+    # cross = Sign(CROSS_IMAGE, (, x))
+    # group.add(circle)
+    # group.add(cross)
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos_x, pos_y = event.pos
+                print(f"pos_x {pos_x}, pos_y {pos_y}")
+                if not (pos_x <= board.size and pos_y <= board.size):
+                    continue
+                (_, _, width, height) = board.surface.get_rect()
+                col = int(pos_x // (width/3))
+                row = int(pos_y // (height/3))
+                print(f"row {row}, col {col}")
+
+                if board.is_valid_move(row, col):
+                    current_player = player
+                    sign = Sign(sign_mapping[current_player], (col*CELL_DELTA+CELL_ORIGIN, row*CELL_DELTA+CELL_ORIGIN))
+                    player = board.move(row, col, current_player)
+                    group.add(sign)
+                    group.add(crosshair)
+                    print(board.status)
+                    if board.is_player_win(current_player):
+                        print(f"Player {current_player} wins")
+                        time.sleep(3)
+                        sys.exit()
+                    if board.is_board_filled():
+                        print("Draw")
+                        time.sleep(3)
+                        sys.exit()
+
+        screen.blit(background, (0, 0))
+        screen.blit(board.surface, (0, 0))
+        group.draw(screen)
+        group.update()
+        clock.tick(60)
+        pygame.display.flip()
